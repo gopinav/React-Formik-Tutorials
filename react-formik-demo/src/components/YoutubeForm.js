e32dfcd2619be70016d52f52b95298a9f3f21024
Vishwas <gopinav5@gmail.com>
2020-04-03 11:52:11 +0530

    validate function

--- react-formik-demo/src/components/YoutubeForm.js
+++ react-formik-demo/src/components/YoutubeForm.js
@@ -1,20 +1,51 @@
 import React from 'react'
 import { useFormik } from 'formik'
 
+const initialValues = {
+  name: 'Vishwas',
+  email: '',
+  channel: ''
+}
+
+const onSubmit = values => {
+  console.log('Form data', values)
+}
+
+const validate = values => {
+  // values.name values.email values.channel
+  const errors = {}
+  // errors.name errors.email and errors.channel
+  // errors.name = 'This field is required'
+
+  if (!values.name) {
+    errors.name = 'Required'
+  }
+
+  if (!values.email) {
+    errors.name = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email format'
+  }
+
+  if (!values.channel) {
+    errors.name = 'Required'
+  }
+
+  return errors
+}
+
 function YoutubeForm () {
   const formik = useFormik({
-    initialValues: {
-      name: 'Vishwas',
-      email: '',
-      channel: ''
-    }
+    initialValues,
+    onSubmit,
+    validate
   })
 
-  console.log(formik.values)
+  // console.log(formik.values)
 
   return (
     <div>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label htmlFor='name'>Name</label>
         <input
           type='text'
