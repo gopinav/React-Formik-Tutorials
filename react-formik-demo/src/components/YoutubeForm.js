109d0f369003496441442d8ea848295023cfbdc3
Vishwas <gopinav5@gmail.com>
2020-04-05 13:42:26 +0530

    Validation with yup

--- react-formik-demo/src/components/YoutubeForm.js
+++ react-formik-demo/src/components/YoutubeForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const initialValues = {
   name: 'Vishwas',
@@ -34,11 +35,20 @@ const validate = values => {
   return errors
 }
 
+const validationSchema = Yup.object({
+  name: Yup.string().required('Required'),
+  email: Yup.string()
+    .email('Invalid email format')
+    .required('Required'),
+  channel: Yup.string().required('Required')
+})
+
 function YoutubeForm () {
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate
+    // validate,
+    validationSchema
   })
 
   console.log('formik.touched', formik.touched)
