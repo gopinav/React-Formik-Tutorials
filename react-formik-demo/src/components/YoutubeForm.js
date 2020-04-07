dc776521ea20717a30d4edd9418a65a4ac3c6db9
Vishwas <gopinav5@gmail.com>
2020-04-07 13:27:30 +0530

    ErrorMessage component

--- react-formik-demo/src/components/YoutubeForm.js
+++ react-formik-demo/src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 
 const initialValues = {
@@ -31,25 +31,19 @@ function YoutubeForm () {
         <div className='form-control'>
           <label htmlFor='name'>Name</label>
           <Field type='text' id='name' name='name' />
-          {formik.touched.name && formik.errors.name ? (
-            <div className='error'>{formik.errors.name}</div>
-          ) : null}
+          <ErrorMessage name='name' />
         </div>
 
         <div className='form-control'>
           <label htmlFor='email'>E-mail</label>
           <Field type='email' id='email' name='email' />
-          {formik.touched.email && formik.errors.email ? (
-            <div className='error'>{formik.errors.email}</div>
-          ) : null}
+          <ErrorMessage name='email' />
         </div>
 
         <div className='form-control'>
           <label htmlFor='channel'>Channel</label>
           <Field type='text' id='channel' name='channel' />
-          {formik.touched.channel && formik.errors.channel ? (
-            <div className='error'>{formik.errors.channel}</div>
-          ) : null}
+          <ErrorMessage name='channel' />
         </div>
 
         <button type='submit'>Submit</button>
