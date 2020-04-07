551c9caa7cefdf269fdab8471bb2efe0942a7b53
Vishwas <gopinav5@gmail.com>
2020-04-07 09:51:17 +0530

    Field component

--- react-formik-demo/src/components/YoutubeForm.js
+++ react-formik-demo/src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Formik, Form } from 'formik'
+import { Formik, Form, Field } from 'formik'
 import * as Yup from 'yup'
 
 const initialValues = {
@@ -30,12 +30,7 @@ function YoutubeForm () {
       <Form>
         <div className='form-control'>
           <label htmlFor='name'>Name</label>
-          <input
-            type='text'
-            id='name'
-            name='name'
-            {...formik.getFieldProps('name')}
-          />
+          <Field type='text' id='name' name='name' />
           {formik.touched.name && formik.errors.name ? (
             <div className='error'>{formik.errors.name}</div>
           ) : null}
@@ -43,12 +38,7 @@ function YoutubeForm () {
 
         <div className='form-control'>
           <label htmlFor='email'>E-mail</label>
-          <input
-            type='email'
-            id='email'
-            name='email'
-            {...formik.getFieldProps('email')}
-          />
+          <Field type='email' id='email' name='email' />
           {formik.touched.email && formik.errors.email ? (
             <div className='error'>{formik.errors.email}</div>
           ) : null}
@@ -56,12 +46,7 @@ function YoutubeForm () {
 
         <div className='form-control'>
           <label htmlFor='channel'>Channel</label>
-          <input
-            type='text'
-            id='channel'
-            name='channel'
-            {...formik.getFieldProps('channel')}
-          />
+          <Field type='text' id='channel' name='channel' />
           {formik.touched.channel && formik.errors.channel ? (
             <div className='error'>{formik.errors.channel}</div>
           ) : null}
