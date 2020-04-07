417d646a82f6ffe01775470339b2ce2a114f45c8
Vishwas <gopinav5@gmail.com>
2020-04-07 13:34:10 +0530

    Added back validate function to old form

--- react-formik-demo/src/components/OldYoutubeForm.js
+++ react-formik-demo/src/components/OldYoutubeForm.js
@@ -12,6 +12,26 @@ const onSubmit = values => {
   console.log('Form data', values)
 }
 
+// const validate = values => {
+//   const errors = {}
+
+//   if (!values.name) {
+//     errors.name = 'Required'
+//   }
+
+//   if (!values.email) {
+//     errors.email = 'Required'
+//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//     errors.email = 'Invalid email format'
+//   }
+
+//   if (!values.channel) {
+//     errors.channel = 'Required'
+//   }
+
+//   return errors
+// }
+
 const validationSchema = Yup.object({
   name: Yup.string().required('Required'),
   email: Yup.string()
