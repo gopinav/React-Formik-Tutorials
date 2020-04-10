2ceaccc3e3607e87747513dec75c4cc19d18cf32
Vishwas <gopinav5@gmail.com>
2020-04-10 13:51:17 +0530

    Field revisited

--- react-formik-demo/src/components/YoutubeForm.js
+++ react-formik-demo/src/components/YoutubeForm.js
@@ -5,7 +5,9 @@ import * as Yup from 'yup'
 const initialValues = {
   name: 'Vishwas',
   email: '',
-  channel: ''
+  channel: '',
+  comments: '',
+  address: ''
 }
 
 const onSubmit = values => {
@@ -35,15 +37,39 @@ function YoutubeForm () {
         </div>
 
         <div className='form-control'>
-          <label htmlFor='email'>E-mail</label>
+          <label htmlFor='email'>Email</label>
           <Field type='email' id='email' name='email' />
           <ErrorMessage name='email' />
         </div>
 
         <div className='form-control'>
           <label htmlFor='channel'>Channel</label>
-          <Field type='text' id='channel' name='channel' />
+          <Field
+            type='text'
+            id='channel'
+            name='channel'
+            placeholder='YouTube channel name'
+          />
           <ErrorMessage name='channel' />
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='comments'>Comments</label>
+          <Field as='textarea' id='comments' name='comments' />
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='address'>Address</label>
+          <Field name='address'>
+            {({ field, form, meta }) => {
+              return (
+                <div>
+                  <input type='text' {...field} />
+                  {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                </div>
+              )
+            }}
+          </Field>
         </div>
 
         <button type='submit'>Submit</button>
