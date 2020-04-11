6614d9c93d9413c0aaf9eeee389d1d063c35155e
Vishwas <gopinav5@gmail.com>
2020-04-11 11:13:22 +0530

    ErrorMessage revisited

--- react-formik-demo/src/components/YoutubeForm.js
+++ react-formik-demo/src/components/YoutubeForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
+import TextError from './TextError'
 
 const initialValues = {
   name: 'Vishwas',
@@ -33,13 +34,17 @@ function YoutubeForm () {
         <div className='form-control'>
           <label htmlFor='name'>Name</label>
           <Field type='text' id='name' name='name' />
-          <ErrorMessage name='name' />
+          <ErrorMessage name='name' component={TextError} />
         </div>
 
         <div className='form-control'>
           <label htmlFor='email'>Email</label>
           <Field type='email' id='email' name='email' />
-          <ErrorMessage name='email' />
+          <ErrorMessage name='email'>
+            {
+              error => <div className='error'>{error}</div>
+            }
+          </ErrorMessage>
         </div>
 
         <div className='form-control'>
