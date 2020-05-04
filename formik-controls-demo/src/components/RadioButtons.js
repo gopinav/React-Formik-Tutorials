acc12a6122924d5f52bc62a5abf34d2c3fbdb486
Vishwas <gopinav5@gmail.com>
2020-05-04 19:07:50 +0530

    Add radio button

--- formik-controls-demo/src/components/RadioButtons.js
+++ formik-controls-demo/src/components/RadioButtons.js
@@ -6,16 +6,16 @@ function RadioButtons (props) {
   const { label, name, options, formik, ...rest } = props
   return (
     <div className='form-control'>
-      <label htmlFor={name}>{label}</label>
-      <Field id={name} name={name} {...rest}>
+      <label>{label}</label>
+      <Field name={name} {...rest}>
         {({ field }) => {
           console.log(field)
           return options.map(option => {
             return (
               <React.Fragment key={option.key}>
                 <input
-                  id={option.value}
                   type='radio'
+                  id={option.value}
                   {...field}
                   value={option.value}
                   checked={field.value === option.value}
