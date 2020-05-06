2cca2bb68e630b342282a80e61dba2a172f26521
Vishwas <gopinav5@gmail.com>
2020-05-06 13:17:54 +0530

    Add LoginForm

--- formik-controls-demo/src/App.js
+++ formik-controls-demo/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import FormikContainer from './components/FormikContainer'
+import LoginForm from './components/LoginForm'
 
 function App () {
   return (
     <div className='App'>
-      <FormikContainer />
+      {/* <FormikContainer /> */}
+      <LoginForm />
     </div>
   )
 }
---
+++ formik-controls-demo/src/components/LoginForm.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import FormikControl from './FormikControl'
+
+function LoginForm () {
+  const initialValues = {
+    email: '',
+    password: ''
+  }
+
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email('Invalid email format')
+      .required('Required'),
+    password: Yup.string().required('Required')
+  })
+
+  const onSubmit = values => {
+    console.log('Form data', values)
+  }
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {formik => {
+        return (
+          <Form>
+            <FormikControl
+              control='input'
+              type='email'
+              label='Email'
+              name='email'
+            />
+            <FormikControl
+              control='input'
+              type='password'
+              label='Password'
+              name='password'
+            />
+            <button type='submit' disabled={!formik.isValid}>Submit</button>
+          </Form>
+        )
+      }}
+    </Formik>
+  )
+}
+
+export default LoginForm
