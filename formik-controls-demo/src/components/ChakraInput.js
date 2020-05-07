1c96b3be68a65fb05e0b8488a37c4c662a036498
Vishwas <gopinav5@gmail.com>
2020-05-07 09:13:16 +0530

    Remove unwanted FormControl wrapper

--- formik-controls-demo/src/components/ChakraInput.js
+++ formik-controls-demo/src/components/ChakraInput.js
@@ -10,17 +10,15 @@ import {
 function ChakraInput (props) {
   const { label, name, ...rest } = props
   return (
-    <FormControl>
-      <Field name={name}>
-        {({ field, form }) => (
-          <FormControl isInvalid={form.errors[name] && form.touched[name]}>
-            <FormLabel htmlFor={name}>{label}</FormLabel>
-            <Input id={name} {...rest} {...field} />
-            <FormErrorMessage>{form.errors[name]}</FormErrorMessage>
-          </FormControl>
-        )}
-      </Field>
-    </FormControl>
+    <Field name={name}>
+      {({ field, form }) => (
+        <FormControl isInvalid={form.errors[name] && form.touched[name]}>
+          <FormLabel htmlFor={name}>{label}</FormLabel>
+          <Input id={name} {...rest} {...field} />
+          <FormErrorMessage>{form.errors[name]}</FormErrorMessage>
+        </FormControl>
+      )}
+    </Field>
   )
 }
 
